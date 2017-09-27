8477d2481a2a7443e10a8dd7bc78606e40d800d9
tbrams <torben@brams.dk>
2017-09-27 17:14:50 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,213 @@
+const express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', 'public');
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    pageData: '[1,2,3]',
+    pageTitle: 'VERY Different!'
+  });
+})
+
+
+app.get('/api/notam', function(req, res) {
+  res.json(orgmap);
+});
+
+
+app.listen(3000);
+
+console.log('Listening on port 3000');
+
+
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+
+function parsePage() {
+
+
+  const place = 'EKDK';
+  const page =
+    "https://pilotweb.nas.faa.gov/PilotWeb/notamRetrievalByICAOAction.do?method=displayByICAOs&reportType=RAW&formatType=ICAO&actionType=notamRetrievalByICAOs&retrieveLocId=" +
+    place;
+
+  var notams = [];
+
+  request(page, function(err, res, body) {
+    if (err) {
+      console.log("Error: ", err.description);
+    } else {
+
+      var $ = cheerio.load(body);
+      $(' div#notamRight', 'div#resultsHomeLeft').each(function(i, elem) {
+        notams[i] = $(this).find('pre').html();
+      });
+    }
+
+    process_notams(notams);
+
+  });
+}
+
+
+
+var notam_objects = [];
+var orgmap = [];
+
+orgmap.push(createMapObject('EKRK', 55.590385, 12.129340));
+orgmap.push(createMapObject('EKCH', 55.618024, 12.650763));
+orgmap.push(createMapObject('EKOD', 55.473886, 10.329182));
+orgmap.push(createMapObject('EKEB', 55.525833, 8.553333));
+
+function process_notams(notams) {
+
+  let ignored = 0;
+  let errors = 0;
+  let count = notams.length;
+  for (var i = 0; i < count; i++) {
+    let notam = notams[i];
+    console.log('Raw notam: ' + notam + '\n');
+
+    let lines = [];
+    lines = notam.split('\n');
+
+    // check first line - should we ignore?
+    if (lines[0].trim()[0] == 'M') {
+      console.log('This notam will be ignored');
+      ignored++;
+    } else {
+
+
+      // Get to the Q-code on line 2
+      let qParts = lines[1].split(' ');
+      let qCode = '';
+      if (qParts[0].toUpperCase() == "Q)") {
+        let qInfoParts = qParts[qParts.length - 1].split('/');
+        qCode = qParts[1].split('/')[1];
+        if (qCode.toUpperCase() == 'QKKKK') {
+          console.log('This notal will be ignored');
+          ignored++;
+        } else {
+          // So we will use the info in this notam
+          // Create object to hold all the info
+          var nObject = {
+            id: lines[0].split(' ')[0],
+            place: '',
+            fromDate: '',
+            toDate: '',
+            qCode: qCode,
+            fromAlt: '',
+            toAlt: '',
+            lat: '',
+            latChar: '',
+            lng: '',
+            lngChar: '',
+            radius: '',
+            text: ''
+          }
+
+          // Spatials
+          let radius = qInfoParts[qInfoParts.length - 1].slice(-3);
+          let coordinates = qInfoParts[qInfoParts.length - 1].slice(0, -3);
+
+          let latChar = 'S';
+          if (coordinates.indexOf('N')) {
+            latChar = 'N';
+          }
+          let cordarr = coordinates.split(latChar);
+
+          let lat = cordarr[0] / 100.;
+          let lng = cordarr[1].slice(0, -1) / 100.;
+          let lonChar = cordarr[1].slice(-1);
+          let toAlt = qInfoParts[qInfoParts.length - 2];
+          let fromAlt = qInfoParts[qInfoParts.length - 3];
+
+          nObject.lat = lat;
+          nObject.lng = lng;
+          nObject.latChar = latChar;
+          nObject.lngChar = lonChar;
+          nObject.fromAlt = fromAlt;
+          nObject.toAlt = toAlt;
+          nObject.radius = radius;
+
+          // Line with A, B and C
+          let line2parts = lines[2].split(' ');
+
+          if (line2parts[0] == 'A)') {
+            let place = line2parts[1];
+            nObject.place = place;
+          }
+
+          if (line2parts[2] == 'B)') {
+            let fromDate = line2parts[3];
+            nObject.fromDate = fromDate;
+          }
+
+          if (line2parts[4] == 'C)') {
+            let toDate = line2parts[5];
+            nObject.toDate = toDate;
+          }
+
+          // Line with E
+          let textLine = 3;
+          if (lines[textLine].substr(0, 2).toUpperCase() == 'E)') {
+            nObject.text = lines[textLine].slice(-lines[3].length + 3);
+
+            // And append the remaining lines, until we find the "CREATED:" signature
+            // or one of the F) Fields we sometimes see in A series notams.
+            while ((lines[++textLine].substr(0, 8).toUpperCase() != "CREATED:") &&
+              (lines[textLine].substr(0, 2).toUpperCase() != "F)")) {
+              nObject.text += '\n' + lines[textLine];
+            }
+          }
+          notam_objects.push(nObject);
+          dump_notam(nObject);
+        }
+
+      } else {
+        console.log("Expected a Q) line here???");
+        errors++;
+      }
+    }
+
+    console.log('\n');
+  }
+
+  console.log(`Processed a total of ${count} notams`);
+  if (ignored > 0)
+    console.log(`Marked ${ignored} as ignored`);
+  if (errors > 0)
+    console.log(`Encountered ${errors}`);
+}
+
+function dump_notam(nObj) {
+  console.log(`Id: ${nObj.id}`);
+  console.log(`Q-code: ${nObj.qCode}`);
+  console.log(`Place: ${nObj.place}`);
+  console.log(`From: ${nObj.fromDate}`);
+  console.log(`To: ${nObj.toDate}`);
+  console.log(
+    `coords: ${nObj.lat}${nObj.latChar} ${nObj.lng}${nObj.lngChar} - radius: ${nObj.radius}`
+  );
+  console.log(`From Alt: ${nObj.fromAlt} to ${nObj.toAlt}`);
+  console.log(`Text: ${nObj.text}`);
+
+  orgmap.push(createMapObject(nObj.place, nObj.lat, nObj.lng))
+}
+
+
+
+function createMapObject(PLACE, LAT, LON) {
+  return {
+    name: PLACE,
+    center: {
+      lat: LAT,
+      lng: LON
+    }
+  }
+}
---
+++ public/map.js
@@ -0,0 +1,71 @@
+var radius = 5000;
+var citymap = [];
+
+function initMap() {
+
+  // Create the map.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {
+      lat: 55.5,
+      lng: 11.0
+    },
+    mapTypeId: 'terrain'
+  });
+  var infoWindow = new google.maps.InfoWindow({
+    content: "<div>Hello! World</div>",
+    maxWidth: 500
+  });
+
+  var counter = 100;
+  $.get('/api/notam', function(data) {
+    var stringData = JSON.stringify(data);
+    citymap = JSON.parse(stringData);
+
+    // Construct the circle for each value in citymap.
+    for (var i = 0; i < citymap.length; i++) {
+      var city = citymap[i];
+      console.log('Adding places');
+
+      // Add the circle for this city to the map.
+      cityCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: city.center,
+        radius: radius
+      });
+
+      addIdToCircle(cityCircle, i, map, infoWindow, counter++);
+
+    }
+
+  });
+}
+
+function addIdToCircle(circle, i, map, infoWindow, id) {
+  // wiring up id's
+  circle['id'] = id;
+  citymap[i]['id'] = id;
+
+  google.maps.event.addListener(circle, 'click', function(ev) {
+    console.log('Clicked on Id: ' + circle.id);
+    for (var c in citymap) {
+      if (citymap[c].id == circle.id) {
+        console.log('Match: ' + citymap[c].id);
+
+        infoWindow.setOptions({
+          content: '<p>' + citymap[c].name + '<br/>Clicked:<br/>(' +
+            ev.latLng.lat().toFixed(3) + ', ' +
+            ev.latLng.lng().toFixed(3) + ')</p>'
+        });
+      }
+
+      infoWindow.setPosition(ev.latLng);
+      infoWindow.open(map);
+    }
+  });
+}
